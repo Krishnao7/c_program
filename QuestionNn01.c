e64cf44c6d8f1ee403f897d3871bbe2425fca6a0
krishna patle <krishnapatle9754@gmail.com>
2025-05-26 11:31:07 +0530

    Add files via upload

---
+++ QuestionNn01.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+int main()
+{
+  int a , b ;
+// inpt for user 
+  printf("enter the value of a:");
+  scanf("%d",&a);
+
+   printf("enter the value of b:");
+   scanf("%d",&b);
+
+int c ,d,e,f;
+   c = a+b;
+   d = a-b;
+   e = a*b;
+   f = a/b;
+
+   printf("sum of two number is = %d\n",c);
+   printf("sbrection of two number is = %d\n",d);
+   printf("multiplaction of two number is = %d\n",e);
+   printf("division of two number is = %d\n",f);
+
+   return 0 ;
+
+
+
+}
