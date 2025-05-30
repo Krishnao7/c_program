a079a71f4ceaa297c25f143ff26bf8377f9eee11
krishna patle <krishnapatle9754@gmail.com>
2025-05-30 18:32:44 +0530

    Add files via upload

---
+++ QuestionNo08.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(){
+
+    int a ;
+    int b ;
+    printf("enter value of a :");
+    scanf("%d",&a);
+    printf("enter value of b :");
+    scanf("%d",&b);
+    if (a==b)
+    {
+        printf("equual");
+    }else
+    {
+         printf("not equal");
+    }
+    
+    return 0;
+}
